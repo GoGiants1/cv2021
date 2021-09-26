a2ab2fda88e6586f64348288767d6e5c461bf0fa
GoGiants1 <chooi221@snu.ac.kr>
2021-09-27 04:18:07 +0900

    Implement median, gaussian filter

--- hw1/HW1_1.py
+++ hw1/HW1_1.py
@@ -140,8 +140,6 @@ def convolve(input_image, Kernel):
 
     reflected_img = reflect_padding(input_image, Kernel.shape)
 
-    # original image matrix size (x, y, z)
-    img_width, img_height, img_channels = image.shape
     # padded img size
     # pad_img_width, pad_img_height, pad_img_channels = reflected_img.shape
     # kernel size and padding size
@@ -149,32 +147,34 @@ def convolve(input_image, Kernel):
     x_pad_size = x_kernel // 2
     y_pad_size = y_kernel // 2
 
-    origin_width, origin_height, _ = input_image.shape
-    out_matrix = np.zeros((origin_width, origin_height, img_channels))
+    # original image matrix size (x, y, z)
+    origin_width, origin_height, origin_channels = input_image.shape
+    out_matrix = np.zeros((origin_width, origin_height, origin_channels))
 
     fliped_kernel = np.flip(Kernel)
-    for i in range(img_channels):
+    for i in range(origin_channels):
         channel = reflected_img[:, :, i]
+        # 원래 이미지 기준에서의 픽셀 좌표
         origin_x, origin_y = 0, 0
         tmp_out = []
-        while origin_x < origin_width:
+        while origin_x < origin_height:
             reflected_x, reflected_y = origin_x + x_pad_size, origin_y + y_pad_size
-            # print(origin_x, origin_y, reflected_x, reflected_y)
-            pix_result = 0
+            pix_result = []
             for r in range(-x_pad_size, x_pad_size + 1):
                 for c in range(-y_pad_size, y_pad_size + 1):
-                    # print(r, c)
-                    pix_result += (
+                    pix_result.append(
                         channel[reflected_x + r, reflected_y + c]
                         * fliped_kernel[r + x_pad_size, c + y_pad_size]
                     )
-            tmp_out.append(pix_result)
+            tmp_out.append(sum(pix_result))
             origin_y += 1
-            if origin_y >= img_width:
+            if origin_y >= origin_width:
                 origin_y = 0
                 origin_x += 1
-            if origin_x >= img_height:
-                out_matrix[:, :, i] = np.array(tmp_out).reshape(img_width, img_height)
+            if origin_x >= origin_height:
+                out_matrix[:, :, i] = np.array(tmp_out).reshape(
+                    origin_width, origin_height
+                )
                 break
 
     return out_matrix
@@ -191,9 +191,42 @@ def median_filter(input_image, size):
     for s in size:
         if s % 2 == 0:
             raise Exception("size must be odd for median filter")
+    origin_width, origin_height, origin_channels = input_image.shape
 
-    # Your code
-    return
+    padded_img = reflect_padding(input_image, size)
+    output_img = np.zeros((origin_width, origin_height, origin_channels))
+    x_filter_size, y_filter_size = size
+    x_pad_size, y_pad_size = x_filter_size // 2, y_filter_size // 2
+
+    for i in range(origin_channels):
+        channel = padded_img[:, :, i]
+
+        origin_x, origin_y = 0, 0
+        tmp_out = []
+        while origin_x < origin_width:
+            reflected_x, reflected_y = origin_x + x_pad_size, origin_y + y_pad_size
+            pixel_vec = []
+            for r in range(-x_pad_size, x_pad_size + 1):
+                pixel_vec = [
+                    *pixel_vec,
+                    *channel[
+                        reflected_x + r,
+                        reflected_y - y_pad_size : reflected_y + y_pad_size + 1,
+                    ],
+                ]
+            tmp_out.append(sorted(pixel_vec)[x_filter_size * y_filter_size // 2 + 1])
+            # tmp_out.append(statistics.median(pixel_vec))
+            origin_y += 1
+            if origin_y >= origin_width:
+                origin_y = 0
+                origin_x += 1
+            if origin_x >= origin_height:
+                output_img[:, :, i] = np.array(tmp_out).reshape(
+                    origin_width, origin_height
+                )
+                break
+
+    return output_img
 
 
 def gaussian_filter(input_image, size, sigmax, sigmay):
@@ -206,24 +239,50 @@ def gaussian_filter(input_image, size, sigmax, sigmay):
     Return:
         Gaussian filtered image (numpy array)
     """
+
+    def gaussian_kernel_1d(sig, f_size):
+        domain = range(-(f_size // 2), f_size // 2 + 1)
+        gaussian_kernel = [
+            (np.exp(-((x ** 2) / (2.0 * sig ** 2))) / (sig * math.sqrt(2.0 * np.pi)))
+            for x in domain
+        ]
+        total_sum = np.sum(gaussian_kernel)
+        return gaussian_kernel / total_sum
+
     # Your code
-    return
+    # padded_img = reflect_padding(input_image, size)
+    print(input_image.shape, size, sigmax, sigmay)
+    origin_width, origin_height, origin_channels = input_image.shape
+    x_filter_size, y_filter_size = size
+    x_kernel = np.array(gaussian_kernel_1d(sigmax, x_filter_size)).reshape(
+        x_filter_size, 1
+    )
+    y_kernel = np.array(gaussian_kernel_1d(sigmay, y_filter_size)).reshape(
+        1, y_filter_size
+    )
+
+    gaussian_kernel_2D = np.dot(x_kernel, y_kernel)
+    print(gaussian_kernel_2D)
+
+    result = convolve(input_image, gaussian_kernel_2D)
+    # result = convolve(result, y_kernel)
+    return result
 
 
 if __name__ == "__main__":
+    # image = np.asarray(
+    #     Image.open(os.path.join(os.getcwd(), "images", "baboon.jpeg")).convert("RGB")
+    # )
     image = np.asarray(
-        Image.open(os.path.join(os.getcwd(), "images", "baboon.jpeg")).convert("RGB")
-    )
-    image_gaussian_noise = np.asarray(
         Image.open(os.path.join(os.getcwd(), "images", "gaussian_noise.jpeg")).convert(
             "RGB"
         )
     )
-    image_pepper_noise = np.asarray(
-        Image.open(
-            os.path.join(os.getcwd(), "images", "salt_and_pepper_noise.jpeg")
-        ).convert("RGB")
-    )
+    # image = np.asarray(
+    #     Image.open(
+    #         os.path.join(os.getcwd(), "images", "salt_and_pepper_noise.jpeg")
+    #     ).convert("RGB")
+    # )
 
     logdir = os.path.join(os.getcwd(), "results", "HW1_1")
     if not os.path.exists(logdir):
@@ -248,18 +307,18 @@ if __name__ == "__main__":
         plt.savefig(os.path.join(logdir, "convolve.jpeg"))
         plt.show()
 
-    # ret = median_filter(image.copy(), kernel_1.shape)
-    # if ret is not None:
-    #     plt.figure()
-    #     plt.imshow(ret.astype(np.uint8))
-    #     plt.axis("off")
-    #     plt.savefig(os.path.join(logdir, "median.jpeg"))
-    #     plt.show()
+    ret = median_filter(image.copy(), kernel_1.shape)
+    if ret is not None:
+        plt.figure()
+        plt.imshow(ret.astype(np.uint8))
+        plt.axis("off")
+        plt.savefig(os.path.join(logdir, "median.jpeg"))
+        plt.show()
 
-    # ret = gaussian_filter(image.copy(), kernel_1.shape, sigmax, sigmay)
-    # if ret is not None:
-    #     plt.figure()
-    #     plt.imshow(ret.astype(np.uint8))
-    #     plt.axis("off")
-    #     plt.savefig(os.path.join(logdir, "gaussian.jpeg"))
-    #     plt.show()
+    ret = gaussian_filter(image.copy(), kernel_1.shape, sigmax, sigmay)
+    if ret is not None:
+        plt.figure()
+        plt.imshow(ret.astype(np.uint8))
+        plt.axis("off")
+        plt.savefig(os.path.join(logdir, "gaussian.jpeg"))
+        plt.show()
